afbb11dd4c51e0b85c9fb43d3c18c7ec3de4d423
Daniel Bulla <d.bulla@fh-aachen.de>
2015-11-25 17:06:39 +0100

    Added documentation

--- commandlinetool_mapman/src/mapman.cpp
+++ commandlinetool_mapman/src/mapman.cpp
@@ -9,17 +9,30 @@ namespace po = boost::program_options;
  * --source=<mapsource>
  *
  * map -l|list
+ *  - list all mapIds
  * map read <mapId>
+ *  - show info about map (name, author, ...)
  * map update <mapId> --name=<name> --<todo>...
+ *  - update info about map (not layerdata update, that would be an <operation>)
  * map remove <mapId>
+ *  - to be discussed
  *
  * layer -l <mapId>
+ *  - shows all layers of a map
  * layer read <layerId>
+ *  - shows info about layer
  * layer update <mapId> --name=<name> --usagetype=<usagetype>
+ *  - update info about layer (not layerdata update, that would be an <operation>)
  * layer remove <mapId> <layerId>
+ *  - to be discussed
  * layer readdata <mapId> <layerId> <minx> <miny> <minz> <maxx> <maxy> <maxz> <clip> <lod>
+ *  - show layerdata (binary chunk of layer) in specific format (e.g. octomap, pcd, ...)
  * layer readdata <mapId> <layerId> <clip> <lod>
+ *  - see above
+ *  - note: transform+timestamp history gets lost/is not retrievable?
+ *  - note: layer readobject <mapId> <layerId> <objectId>
  * layer persist <add|remove> <mapId> <layerId>
+ *  - mark layer as physically stored (for fast retrieval/manipulation)
  *
  * operator <opname> <params...> (<params> includes mostly <mapId...> <layerId...>)
  *
@@ -31,8 +44,8 @@ namespace po = boost::program_options;
  * - layer update has no (--type=<type>)
  * - operator generates mapId(s) and layerId(s) + type + usagetype on demand
  * - maps and layers are created implicitly by operators.
- *   Thus, "map create" should be expressed by something like "operator copy --name=mymap"
- *   To read in rawdata: "operator load /home/.../myPcd.pcd"
+ *   Thus, "map create" should be expressed by something like "operator copy --name=mymap" (copy makes no sense)
+ *   To read rawdata: "operator load /home/.../myPcd.pcd"
  *   - layerdata store <mapId> <layerId> <minx> <miny> <minz> <maxx> <maxy> <maxz> <lod> [datastream cin]|--filename
  *   - layerdata store <clip> <lod> [datastream cin]|--filename
  *
