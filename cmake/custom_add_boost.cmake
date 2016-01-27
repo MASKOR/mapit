06a43eeeccb1a1359294749bd8d8fa6011bc9f86
dbulla <info@danielbulla.de>
2016-01-27 22:17:11 +0100

    CMake cleanups

--- cmake/custom_add_boost.cmake
+++ cmake/custom_add_boost.cmake
@@ -1,6 +1,6 @@
 # How is this framework loaded?
 #  For linux this is easily the version installed by packetmanager.
-#  It should not be needed to mix diferent versions of boost for any OS.
+#  It should not be needed to mix different versions of boost for any OS.
 #
 # How can be compiled manually?
 #  On Windows install pre-built binaries for msvc12 (msvc12 is equal to msvc2013).
@@ -19,11 +19,23 @@
 #  "boost" (dynamic linking). This requires you to put the needed boost*.dll-files next to the binary or in a system
 #  path. (static linking seems to work also, not yet tested enough. Position independend code must then be enabled)
 #  "-gd" are debug libs for boost.
+# CMake:
+#  FindBoost was a pain in the neck and incapable of handling cmake cache variables correctly. This may only be an issue with PCL.
+#  This solution does also not handle the vars correctly but adds a lot of comfort by setting all 14 paths automatically.
+#  Use
+#  find_package(Boost COMPONENTS filesystem system date_time)
+#  After this call.
 
 macro(custom_set_vars_boost)
     if (WIN32)
-        set(BOOST_CUSTOM_PATH "E:\devel\boost_1_56_0\lib64-msvc-12.0" CACHE PATH "let it point to all the libs")
+        set(BOOST_CUSTOM_ROOT "E:/devel/boost_1_56_0" CACHE PATH "let it point to all the root")
+        set(BOOST_CUSTOM_PATH ${BOOST_CUSTOM_ROOT}/lib64-msvc-12.0 CACHE PATH "let it point to all the libs")
+        SET(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH} C:/boost_1_56_0)
+        SET(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} ${BOOST_CUSTOM_PATH})
+        set(BOOST_LIBRARYDIR ${BOOST_CUSTOM_PATH})
+        set(BOOST_INCLUDEDIR ${BOOST_CUSTOM_ROOT})
         if(BOOST_CUSTOM_PATH)
+            set(Boost_DIR "${BOOST_CUSTOM_PATH}")
             set(Boost_CHRONO_LIBRARY_DEBUG "${BOOST_CUSTOM_PATH}/libboost_chrono-vc120-mt-gd-1_56.lib")
             set(Boost_CHRONO_LIBRARY_RELEASE "${BOOST_CUSTOM_PATH}/libboost_chrono-vc120-mt-1_56.lib")
             set(Boost_DATE_TIME_LIBRARY_DEBUG "${BOOST_CUSTOM_PATH}/libboost_date_time-vc120-mt-gd-1_56.lib")
@@ -38,8 +50,8 @@ macro(custom_set_vars_boost)
             set(Boost_SYSTEM_LIBRARY_RELEASE "${BOOST_CUSTOM_PATH}/libboost_system-vc120-mt-1_56.lib")
             set(Boost_THREAD_LIBRARY_DEBUG "${BOOST_CUSTOM_PATH}/libboost_thread-vc120-mt-gd-1_56.lib")
             set(Boost_THREAD_LIBRARY_RELEASE "${BOOST_CUSTOM_PATH}/libboost_thread-vc120-mt-1_56.lib")
-        endif(BOOST_CUSTOM_PATH)
 
+        endif(BOOST_CUSTOM_PATH)
     else()
 
     endif()
@@ -48,6 +60,5 @@ macro(custom_set_vars_boost)
 endmacro(custom_set_vars_boost)
 
 macro(custom_target_use_boost TARGET)
-    custom_add_vars_leveldb()
-    target_link_library(${TARGET} ${CUSTOM_LIBRARIES_LEVELDB})
+    target_link_libraries(${TARGET} ${CUSTOM_LIBRARIES_LEVELDB})
 endmacro(custom_target_use_boost)
