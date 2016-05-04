3fe7225d8750c840b01838c6e92395bc884e17cf
Tobias Neumann <t.neumann@fh-aachen.de>
2016-05-04 13:56:20 +0200

    logging: fixed loading of properties and add color (LINUX only?)

--- libs/mapmanager/include/upns_globals.h
+++ libs/mapmanager/include/upns_globals.h
@@ -23,17 +23,26 @@
 
 #include <log4cplus/logger.h>
 #include <log4cplus/loggingmacros.h>
-#ifdef UPNS_DEBUG
-    #ifdef USE_QT_STRUCTURES
-        #define log_error(msg) do{LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + msg); qassert_x(false, "upns", msg); break;}while(true)
-    #else
-        #define log_error(msg) do{LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + msg); assert(false); break;}while(true)
-    #endif
-#else // UPNS_DEBUG
-    #define log_error(msg) LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + msg)
-#endif // UPNS_DEBUG
-#define log_warn(msg) LOG4CPLUS_WARN(log4cplus::Logger::getInstance("main"), std::string() + msg)
-#define log_info(msg) LOG4CPLUS_INFO(log4cplus::Logger::getInstance("main"), std::string() + msg)
+//#ifdef UPNS_DEBUG
+//    #ifdef USE_QT_STRUCTURES
+//        #define log_error(msg) do{LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + msg); qassert_x(false, "upns", msg); break;}while(true)
+//    #else
+//        #define log_error(msg) do{LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + msg); assert(false); break;}while(true)
+//    #endif
+//#else // UPNS_DEBUG
+//    #define log_error(msg) LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + "\033[0;31m" + msg + "\033[0m")
+//#endif // UPNS_DEBUG
+
+//#if defined(_MSC_VER) // Microsoft compiler
+//  #define log_error(msg) LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + msg)
+//  #define log_warn(msg) LOG4CPLUS_WARN(log4cplus::Logger::getInstance("main"), std::string() + msg)
+//  #define log_info(msg) LOG4CPLUS_INFO(log4cplus::Logger::getInstance("main"), std::string() + msg)
+//#elif defined(__GNUC__) // GNU compiler
+  #define log_fatal(msg) LOG4CPLUS_FATAL(log4cplus::Logger::getInstance("main"), std::string() + "\033[41m" + msg + "\033[0m")
+  #define log_error(msg) LOG4CPLUS_ERROR(log4cplus::Logger::getInstance("main"), std::string() + "\033[1;31m" + msg + "\033[0m")
+  #define log_warn(msg) LOG4CPLUS_WARN(log4cplus::Logger::getInstance("main"), std::string() + "\033[1;33m" + msg + "\033[0m")
+  #define log_info(msg) LOG4CPLUS_INFO(log4cplus::Logger::getInstance("main"), std::string() + "\033[1;32m" + msg + "\033[0m")
+//#endif
 
 namespace upns
 {
--- test/unit_tests/src/testmain.cpp
+++ test/unit_tests/src/testmain.cpp
@@ -6,6 +6,7 @@
 
 int main(int argc, char *argv[])
 {
+  /*
     log4cplus::BasicConfigurator config;
     config.configure();
     log4cplus::SharedAppenderPtr consoleAppender(new log4cplus::ConsoleAppender());
@@ -13,6 +14,9 @@ int main(int argc, char *argv[])
     //consoleAppender->setLayout(std::auto_ptr<log4cplus::Layout>(new log4cplus::TTCCLayout()));
     log4cplus::Logger mainLogger = log4cplus::Logger::getInstance("main");
     mainLogger.addAppender(consoleAppender);
+*/
+    log4cplus::PropertyConfigurator::doConfigure("logging.properties");
+
     log_info("running test");
     return AutoTest::run(argc, argv);
 }
