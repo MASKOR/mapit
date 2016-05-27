48e0f2f3104ada9bd1942108e85fae49932b41d7
Daniel Bulla <d.bulla@fh-aachen.de>
2016-05-27 18:29:03 +0200

    Minor changes gui

--- tools/visualization_renderview/src/bindings/qmlcheckout.cpp
+++ tools/visualization_renderview/src/bindings/qmlcheckout.cpp
@@ -6,6 +6,56 @@ QString QmlCheckout::doOperation(QString operatorname, const QJsonObject &desc)
     return "";
 }
 
+void QmlCheckout::setConflictSolved(QString path, QString oid)
+{
+    m_checkout->setConflictSolved(path, oid);
+}
+
+QmlTree *QmlCheckout::getRoot()
+{
+
+}
+
+QmlTree *QmlCheckout::getTreeConflict(QString objectId)
+{
+
+}
+
+QmlEntity *QmlCheckout::getEntityConflict(QString objectId)
+{
+
+}
+
+QmlTree *QmlCheckout::getTree(QString path)
+{
+
+}
+
+QmlEntity *QmlCheckout::getEntity(QString path)
+{
+
+}
+
+QmlBranch *QmlCheckout::getParentBranch()
+{
+
+}
+
+QString QmlCheckout::getParentCommitIds()
+{
+
+}
+
+QmlEntitydata *QmlCheckout::getEntityDataReadOnly(QString entityId)
+{
+
+}
+
+QmlEntitydata *QmlCheckout::getEntityDataReadOnlyConflict(QString entityId)
+{
+
+}
+
 bool QmlCheckout::isInConflictMode() const
 {
     return m_isInConflictMode;
