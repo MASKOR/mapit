65714cb70f848e35f790cc5f55ee8f49796dd018
Daniel Bulla <d.bulla@fh-aachen.de>
2016-09-26 18:32:14 +0200

    Added raw write operator.

---
+++ libs/operator_modules_collection/write_raw/src/main.cpp
@@ -0,0 +1,20 @@
+#include "module.h"
+#include "libs/layertypes_collection/tf/include/tflayer.h"
+#include "modules/versioning/checkoutraw.h"
+#include "operationenvironment.h"
+#include <iostream>
+#include <memory>
+#include "error.h"
+#include "modules/versioning/checkoutraw.h"
+
+upns::StatusCode operate(upns::OperationEnvironment* env)
+{
+    assert(false); // Not yet implemented
+    OperationDescription out;
+    out.set_operatorname(OPERATOR_NAME);
+    out.set_operatorversion(OPERATOR_VERSION);
+    env->setOutputDescription( out.SerializeAsString() );
+    return UPNS_STATUS_OK;
+}
+
+UPNS_MODULE(OPERATOR_NAME, "just write raw data", "fhac", OPERATOR_VERSION, upns::LayerType::ANY_RAW, &operate)
