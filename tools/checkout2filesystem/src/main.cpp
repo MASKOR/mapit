e7589919113de4202676a1ce14a37f34cc97a5e4
Tobias Neumann <t.neumann@fh-aachen.de>
2016-12-01 13:28:19 +0100

    tools/checkout2filesystem: moved from qt to boost

--- tools/checkout2filesystem/src/main.cpp
+++ tools/checkout2filesystem/src/main.cpp
@@ -3,11 +3,16 @@
 #include "services.pb.h"
 #include "versioning/repository.h"
 #include "versioning/repositoryfactory.h"
-#include <QFile>
-#include <QDir>
+#include <boost/filesystem.hpp>
+#include <boost/iostreams/device/mapped_file.hpp>
 #include <yaml-cpp/yaml.h>
 #include <log4cplus/configurator.h>
 #include <log4cplus/consoleappender.h>
+#include <fstream>
+#include <sstream>
+
+namespace fs = boost::filesystem;
+namespace iostr = boost::iostreams;
 
 int main(int argc, char *argv[])
 {
@@ -29,35 +34,24 @@ int main(int argc, char *argv[])
 
     upns::upnsSharedPointer<upns::Checkout> co = repo->getCheckout(argv[2]);
 
+    if (co == NULL) {
+        upns::upnsVec<upns::upnsString> possibleCheckouts = repo->listCheckoutNames();
+        if (possibleCheckouts.size() == 0) {
+            log_info("No possible checkout");
+        }
+        for ( upns::upnsString checkout : possibleCheckouts ) {
+            log_info("Possible checkout: " + checkout);
+        }
+        return 1;
+    }
+
     upns::upnsSharedPointer<upns::Tree> currentDirectory(co->getRoot());
-    std::string rootPath(argv[3]);
-    if(rootPath.at(rootPath.length()-1) != '/')
-    {
-        // ensure trailing "/"
-        rootPath += "/";
-    }
-    std::string::size_type idx = rootPath.find_last_of('/', rootPath.length()-2);
-    std::string folderName;
-    // get last segment of path
-    if(idx == std::string::npos)
-    {
-        folderName = rootPath;
-    }
-    else
-    {
-        folderName = rootPath.substr(idx, rootPath.length()-idx-1);
-    }
-    // if last segment is equal to checkoutname, use it.
-    if(folderName != argv[2])
-    {
+    fs::path rootPath(argv[3]);
+    if ( rootPath.filename() != argv[2] ) {
         // otherwise, use subfolder with checkout name
-        rootPath += argv[2];
+        rootPath /= fs::path( argv[2] );
     }
-    if(rootPath.at(rootPath.length()-1) == '/')
-    {
-        // ensure trailing "/" again
-        rootPath = rootPath.substr(0, rootPath.length()-1);
-    }
+
     upns::StatusCode s = co->depthFirstSearch([&](
         upns::upnsSharedPointer<upns::Commit> obj, const upns::ObjectId& oid, const upns::Path &path)
         {
@@ -69,11 +63,10 @@ int main(int argc, char *argv[])
         },
         [&](upns::upnsSharedPointer<upns::Tree> obj, const upns::ObjectId& oid, const upns::Path &path)
         {
-            QDir current(QString::fromStdString(rootPath));
-            if(!path.empty() && !current.exists(QString::fromStdString(path)))
-            {
-                if(!current.mkpath(QString::fromStdString(path.substr(1, path.length()-1))))
-                {
+            fs::path current( rootPath );
+            fs::path path_new = rootPath / fs::path(path);
+            if( ! path.empty() && ! fs::exists( path_new ) ) {
+                if ( ! fs::create_directories(path_new) ) {
                     log_error("Path " + path + " could not be created");
                     return false;
                 }
@@ -85,30 +78,67 @@ int main(int argc, char *argv[])
         },
         [&](upns::upnsSharedPointer<upns::Entity> obj, const upns::ObjectId& oid, const upns::Path &path)
         {
-            QFile current(QString::fromStdString(rootPath + path));
-            if(current.exists())
-            {
+            fs::path current(rootPath / fs::path(path));
+            if ( fs::exists( current ) ) {
                  //QByteArray ar = current.readAll();
                  //Hash
+            } else {
             }
-            else
-            {
+
+            // get the stream to write into a file
+            upns::upnsSharedPointer<upns::AbstractEntitydata> reader = co->getEntitydataReadOnly(path);
+            upns::upnsIStream *entityStream = reader->startReadBytes();
+
+            // calculate the step size to write into the file
+            size_t entitySize = reader->size();
+            size_t offsetMax = 4 * 1024 * 1024; // 4 MB // what makes sense? 100MB?
+            size_t offsetStep = entitySize;
+            if ( offsetStep > offsetMax ) {
+                offsetStep = offsetMax;
             }
-            if(current.open(QFile::WriteOnly))
-            {
-                upns::upnsSharedPointer<upns::AbstractEntitydata> reader = co->getEntitydataReadOnly(path);
-                upns::upnsIStream *istrm = reader->startReadBytes();
-                char buffer[4096];
-                while (istrm->read(buffer, sizeof(buffer)))
-                    current.write(buffer, 4096);
-                current.write(buffer, istrm->gcount());
-                reader->endRead(istrm);
-                current.close();
+
+            // loop to write into the file in step width
+            bool writeIsDone = false;
+            std::ofstream file_out(current.string()/*, std::ofstream::app*/);
+            for (size_t offsetIterator = 0; ! writeIsDone; offsetIterator++) {
+                char* buffer = new char[offsetStep];
+
+                if ( entityStream->read(buffer, offsetStep) ) { // if the end is not reached
+                    file_out.write(buffer, offsetStep);
+                } else {
+                    size_t left = entityStream->gcount();
+                    file_out.write(buffer, left);  // write the left rest
+                    writeIsDone = true; // stop writing with next loop
+                }
+                delete[] buffer;
+
+//                // open a mapped file of size of step
+//                iostr::stream<iostr::mapped_file_sink> file;
+//                file.open( iostr::mapped_file_sink(current),
+//                           offsetStep,
+//                           offsetIterator * offsetStep,
+//                           iostr::mapped_file::readwrite );
+
+
+//                iostr::mapped_file_sink file;
+//                file.open(current,
+//                          offsetStep,
+//                          offsetIterator * offsetStep,
+//                          iostr::mapped_file::readwrite);
+//                if ( file.is_open() ) {
+//                    // write into the file
+//                    if ( ! entityStream->read(file.data(), offsetStep) ) { // if the end is reached
+//                        writeIsDone = true; // stop writing with next loop
+//                    }
+
+//                    file.close();
+//                } else {
+//                    log_error("could not write entity: " + path);
+//                }
             }
-            else
-            {
-                log_error("could not write entity: " + path);
-            }
+            reader->endRead(entityStream);
+            file_out.close();
+
             return true;
         },
         [&](upns::upnsSharedPointer<upns::Entity> obj, const upns::ObjectId& oid, const upns::Path &path)
