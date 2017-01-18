80bdb8690ba3ede354ef70b06c2638d7ac32971d
Daniel Bulla <d.bulla@fh-aachen.de>
2017-01-18 11:21:14 +0100

    removed debug output from centroid_to_origin operator

--- libs/operator_modules_collection/centroid_to_origin/src/main.cpp
+++ libs/operator_modules_collection/centroid_to_origin/src/main.cpp
@@ -24,34 +24,13 @@ upns::StatusCode operate_ctr(upns::OperationEnvironment* env)
     upnsPointcloud2Ptr pc2 = entityData->getData();
     pcl::PointCloud<pcl::PointXYZ> pc; // TODO: make generic
     pcl::fromPCLPointCloud2(*pc2, pc);
-    Eigen::Vector4f centroid;
-//    pcl::compute3DCentroid(pc, centroid);
-//    log_warn("DBG, centroid ctr: " + std::to_string(centroid[0]) + ", " + std::to_string(centroid[1]) + ", " + std::to_string(centroid[2]));
 
     pcl::PointXYZ min;
     pcl::PointXYZ max;
     pcl::getMinMax3D(pc, min, max);
     Eigen::Vector4f ctr((min.x+max.x)/2.0f, (min.y+max.y)/2.0f, (min.z+max.z)/2.0f, 1.0f);
-    //pcl::PointXYZ ctr((min.x+max.x)/2.0f, (min.y+max.y)/2.0f, (min.z+max.z)/2.0f);
     pcl::PointCloud<pcl::PointXYZ> pcCtr; // TODO: make generic
     pcl::demeanPointCloud(pc, ctr, pcCtr);
-
-//    //TODO: Bounding box! dann diese zentrieren. Ausreißer löschen
-////    Eigen::Vector3f ctr = centroid.head<3>() * -1.0f;
-////    Eigen::Quaternionf q;
-//    //pcl::transformPointCloud(pc, pcCtr, ctr, q);
-////    centroid *= -1.0f;
-//    pcl::demeanPointCloud(pc, ctr, pcCtr);
-    pcl::compute3DCentroid(pcCtr, centroid);
-    log_warn("DBG, works" + " ctr: " + std::to_string(centroid[0]) + ", " + std::to_string(centroid[1]) + ", " + std::to_string(centroid[2]));
-
-    pcl::demeanPointCloud(pcCtr, centroid, pc);
-    pcl::compute3DCentroid(pc, centroid);
-    log_warn("DBG, works2" + " ctr: " + std::to_string(centroid[0]) + ", " + std::to_string(centroid[1]) + ", " + std::to_string(centroid[2]));
-
-//    pcl::demeanPointCloud(pc, centroid, pcCtr);
-//    pcl::compute3DCentroid(pcCtr, centroid);
-//    log_warn("DBG, works3" + " ctr: " + std::to_string(centroid[0]) + ", " + std::to_string(centroid[1]) + ", " + std::to_string(centroid[2]));
 
     upnsPointcloud2Ptr pc2Out(new pcl::PCLPointCloud2());
     pcl::toPCLPointCloud2( pc, *pc2Out);
