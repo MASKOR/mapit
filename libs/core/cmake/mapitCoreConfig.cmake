2a4abc6759170b4622be577535340a34beb93d09
Daniel Bulla <d.bulla@fh-aachen.de>
2017-06-07 18:46:40 +0200

    Deployment: core, interface and interface_cpp is now usable outside of cmake-project (*Config.cmake)

--- libs/core/cmake/mapitCoreConfig.cmake
+++ libs/core/cmake/mapitCoreConfig.cmake
@@ -7,9 +7,13 @@
 #get_filename_component(MAPIT_CORE_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 #set(MAPIT_CORE_INCLUDE_DIRS "@CONF_INCLUDE_DIRS@")
  
+include(CMakeFindDependencyMacro)
+find_dependency(Boost REQUIRED COMPONENTS filesystem iostreams)
+
 # Our library dependencies (contains definitions for IMPORTED targets)
 if(NOT TARGET upns_core AND NOT upns_core_BINARY_DIR)
   include("${MAPIT_CORE_CMAKE_DIR}/mapitCoreTargets.cmake")
+  include("${MAPIT_CORE_CMAKE_DIR}/mapitCoreMacros.cmake")
 endif()
  
 # These are IMPORTED targets created by mapitCoreTargets.cmake
