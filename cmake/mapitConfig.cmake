68c647df273b1aaea916b0585154dc7ae172bbfa
Daniel Bulla <d.bulla@fh-aachen.de>
2017-07-13 14:57:49 +0200

    Deployment: new cmake configuration for all layertypes.

--- cmake/mapitConfig.cmake
+++ cmake/mapitConfig.cmake
@@ -15,6 +15,7 @@ set(MAPIT_CORE_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
 set(WITH_PCL TRUE CACHE BOOL "Enable pcl Layers")
 set(WITH_OPENVDB TRUE CACHE BOOL "Enable OpenVDB Layers")
 set(WITH_OCTOMAP TRUE CACHE BOOL "Enable Octomap Layers")
+set(WITH_LAS TRUE CACHE BOOL "Enable LAS Layers")
 set(WITH_TF TRUE CACHE BOOL "Enable Tf Layers, requiring Eigen2")
 
 set(WITH_LOG4CPLUS FALSE CACHE BOOL "Enable logging with log4cplus. Uses standard c out as an alternative")
@@ -26,9 +27,26 @@ if(MAPIT_CMAKE_TARGETS_AVAILABLE)
   set(mapit_interface_cpp interface_cpp)
   set(mapit_standard_repository_factory standard_repository_factory)
   set(mapit_networking_node networking_node)
+### layertypes ###
+  set(mapit_layertype_asset layertype_asset)
+  set(mapit_layertype_boundingbox layertype_boundingbox)
+  set(mapit_layertype_pose_path layertype_pose_path)
+  set(mapit_layertype_primitive layertype_primitive)
   if(WITH_PCL)
     set(mapit_layertype_pointcloud2 layertype_pointcloud2)
   endif(WITH_PCL)
+  if(WITH_OPENVDB)
+    set(mapit_layertype_openvdb layertype_openvdb)
+  endif(WITH_OPENVDB)
+  if(WITH_OCTOMAP)
+    set(mapit_layertype_octomap layertype_octomap)
+  endif(WITH_OCTOMAP)
+  if(WITH_LAS)
+  set(mapit_layertype_las layertype_las)
+  endif(WITH_LAS)
+  if(WITH_TF)
+  set(mapit_layertype_tf layertype_tf)
+  endif(WITH_TF)
 else(MAPIT_CMAKE_TARGETS_AVAILABLE)
   message("mapit targets are IMPORTED (installed or in build dir). Please use mapit::* or \${mapit_*} to refer to mapit targets.")
   # TODO: installed directory structure
@@ -54,6 +72,11 @@ else(MAPIT_CMAKE_TARGETS_AVAILABLE)
   set(mapit_interface_cpp mapit::interface_cpp)
   set(mapit_standard_repository_factory mapit::standard_repository_factory)
   set(mapit_networking_node mapit::networking_node)
+### layertypes ###
+  set(mapit_layertype_asset mapit::layertype_asset)
+  set(mapit_layertype_boundingbox mapit::layertype_boundingbox)
+  set(mapit_layertype_pose_path mapit::layertype_pose_path)
+  set(mapit_layertype_primitive mapit::layertype_primitive)
   if(WITH_PCL)
     set(CMAKE_PREFIX_PATH
       ${CMAKE_CURRENT_LIST_DIR}/libs/layertypes_collection/pointcloud2
@@ -62,6 +85,38 @@ else(MAPIT_CMAKE_TARGETS_AVAILABLE)
     find_dependency(mapit_layertype_pointcloud2 REQUIRED)
     set(mapit_layertype_pointcloud2 mapit::layertype_pointcloud2)
   endif(WITH_PCL)
+  if(WITH_OPENVDB)
+    set(CMAKE_PREFIX_PATH
+      ${CMAKE_CURRENT_LIST_DIR}/libs/layertypes_collection/openvdb
+      ${CMAKE_PREFIX_PATH}
+    )
+    find_dependency(mapit_layertype_openvdb REQUIRED)
+    set(mapit_layertype_openvdb mapit::layertype_openvdb)
+  endif(WITH_OPENVDB)
+  if(WITH_OCTOMAP)
+    set(CMAKE_PREFIX_PATH
+      ${CMAKE_CURRENT_LIST_DIR}/libs/layertypes_collection/octomap
+      ${CMAKE_PREFIX_PATH}
+    )
+    find_dependency(mapit_layertype_octomap REQUIRED)
+    set(mapit_layertype_octomap mapit::layertype_octomap)
+  endif(WITH_OCTOMAP)
+  if(WITH_LAS)
+    set(CMAKE_PREFIX_PATH
+      ${CMAKE_CURRENT_LIST_DIR}/libs/layertypes_collection/las
+      ${CMAKE_PREFIX_PATH}
+    )
+    find_dependency(mapit_layertype_las REQUIRED)
+    set(mapit_layertype_las mapit::layertype_las)
+  endif(WITH_LAS)
+  if(WITH_TF)
+    set(CMAKE_PREFIX_PATH
+      ${CMAKE_CURRENT_LIST_DIR}/libs/layertypes_collection/tf
+      ${CMAKE_PREFIX_PATH}
+    )
+    find_dependency(mapit_layertype_tf REQUIRED)
+    set(mapit_layertype_tf mapit::layertype_tf)
+  endif(WITH_TF)
 endif(MAPIT_CMAKE_TARGETS_AVAILABLE)
 # Our library dependencies (contains definitions for IMPORTED targets)
 #if(NOT TARGET upns_core AND NOT upns_core_BINARY_DIR)
---
+++ libs/layertypes_collection/asset/cmake/mapit_layertype_assetConfig.cmake
@@ -0,0 +1,17 @@
+set(MAPIT_CORE_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
+
+include(CMakeFindDependencyMacro)
+find_dependency(mapit_interface_cpp REQUIRED)
+find_dependency(mapit_core REQUIRED)
+# conditional compiling for different variables set/unset (like WITH_LOG4CPLUS) not supported.
+#if($<WITH_LOG4CPLUS>)
+#  include(FindLog4cplus)
+#  if(LOG4CPLUS_FOUND)
+#    add_definitions(-DLOG4CPLUS_FOUND)
+#  endif(LOG4CPLUS_FOUND)
+#endif($<WITH_LOG4CPLUS>)
+
+# Our library dependencies (contains definitions for IMPORTED targets)
+if(NOT TARGET mapit::layertype_asset AND NOT mapit::layertype_asset_BINARY_DIR)
+  include("${MAPIT_CORE_CMAKE_DIR}/mapit_layertype_assetTargets.cmake")
+endif()
