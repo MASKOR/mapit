4663e9c65cf701dc205bc0c6f3c07431b72c6b88
Tobias Neumann <t.neumann@fh-aachen.de>
2017-10-05 14:54:47 +0200

    operator/transform: changed info contained in a transform
    with the implementation of the tf2 buffer more information within
    transforms are needed, this are done by a new implementation of the
    transform-operator which loades them into the mapit system

--- libs/operator_modules_collection/transform/src/main.cpp
+++ libs/operator_modules_collection/transform/src/main.cpp
@@ -28,116 +28,160 @@
 // absolute:
 // only one tf (index:0) may be given, parent/timestamp must not be used.
 
-template<typename Indexable>
-void jsonToMat(float* d, Indexable &json)
+upns::StatusCode getEntity(upns::OperationEnvironment* env, std::string entity_name, std::shared_ptr<mapit::msgs::Entity> &entity, std::shared_ptr<TfEntitydata> &entityData)
 {
-    //TODO: unroll
-    for(int i=0 ; i<16 ; ++i)
-    {
-        if(json[i].isDouble())
-        {
-            d[i] = static_cast<float>(json[i].toDouble());
-        }
-    }
-}
-
-upns::StatusCode operate(upns::OperationEnvironment* env)
-{
-    QJsonDocument paramsDoc = QJsonDocument::fromJson( QByteArray(env->getParameters().c_str(), env->getParameters().length()) );
-    QJsonObject params(paramsDoc.object());
-
-    std::string target = params["target"].toString().toStdString();
-
-    bool newlyCreated = false; // In this case, there will be no tf to read
     // Get Target
-    std::shared_ptr<mapit::msgs::Entity> tfEntity = env->getCheckout()->getEntity(target);
-    if(tfEntity == NULL)
+    entity = env->getCheckout()->getEntity(entity_name);
+    if(entity == NULL)
     {
         // If target could not be received, create new entity
-        tfEntity = std::shared_ptr<mapit::msgs::Entity>(new mapit::msgs::Entity);
-        tfEntity->set_type(TfEntitydata::TYPENAME());
-        StatusCode s = env->getCheckout()->storeEntity(target, tfEntity);
+        entity = std::shared_ptr<mapit::msgs::Entity>(new mapit::msgs::Entity);
+        entity->set_type(TfEntitydata::TYPENAME());
+        StatusCode s = env->getCheckout()->storeEntity(entity_name, entity);
         if(!upnsIsOk(s))
         {
             log_error("Failed to create entity.");
             return UPNS_STATUS_ERR_UNKNOWN;
         }
-        newlyCreated = true;
     }
-    std::shared_ptr<AbstractEntitydata> abstractEntitydata = env->getCheckout()->getEntitydataForReadWrite( target );
+    else
+    {
+        log_warn("Overwrite existing tf " + entity_name);
+    }
+    std::shared_ptr<AbstractEntitydata> abstractEntitydata = env->getCheckout()->getEntitydataForReadWrite( entity_name );
     if(abstractEntitydata == NULL)
     {
         return UPNS_STATUS_ERR_UNKNOWN;
     }
-    std::shared_ptr<TfEntitydata> entityData = std::dynamic_pointer_cast<TfEntitydata>( abstractEntitydata );
+    entityData = std::dynamic_pointer_cast<TfEntitydata>( abstractEntitydata );
     if(entityData == NULL)
     {
-        log_error("Tf Transform has wrong type.");
+        log_error("Entity is not of type TfEntitydata.");
         return UPNS_STATUS_ERR_UNKNOWN;
     }
-    TfMatPtr tf;
-    if(newlyCreated)
-    {
-        tf = TfMatPtr(new TfMat);
-    }
-    else
-    {
-        tf = entityData->getData();
-    }
-
-    std::string mode = params["mode"].toString().toStdString();
-
-    if(mode == "absolute")
-    {
-        TfMat mat;
-        *tf = TfMat::Identity();
-        float *d = tf->data();
-        if(params["tf"].isObject())
-        {
-            QJsonObject obj(params["tf"].toObject());
-            if(obj["mat"].isArray())
-            {
-                QJsonArray idxbl(obj["mat"].toArray());
-                jsonToMat(d, idxbl);
-            }
-            else
-            {
-                log_error("wrong parameter given, absolute tf has no mat");
-                return UPNS_STATUS_ERR_DB_INVALID_ARGUMENT;
-            }
-        }
-        else if(params["tf"].isArray() && params["tf"].toArray()[0].isObject())
-        {
-            QJsonArray arr(params["tf"].toArray());
-            QJsonObject obj(arr[0].toObject());
-            if(obj["mat"].isArray())
-            {
-                QJsonArray idxbl(obj["mat"].toArray());
-                jsonToMat(d, idxbl);
-            }
-            else
-            {
-                log_error("wrong parameter given, absolute tf[0] has no mat");
-                return UPNS_STATUS_ERR_DB_INVALID_ARGUMENT;
-            }
-        }
-        else
-        {
-            log_error("wrong parameter given, tf is invalid");
-            return UPNS_STATUS_ERR_DB_INVALID_ARGUMENT;
-        }
-    }
-    else if(mode == "relative")
-    {
-    }
-    else
-    {
-        log_error("wrong parameter given, mode was not relative or absolute");
-        return UPNS_STATUS_ERR_DB_INVALID_ARGUMENT;
-    }
-    entityData->setData(tf);
 
     return UPNS_STATUS_OK;
 }
 
-UPNS_MODULE(OPERATOR_NAME, "use pcl normalestimation on a pointcloud", "fhac", OPERATOR_VERSION, "any", &operate)
+void saveEntity(upns::OperationEnvironment* env, std::string entity_name, std::shared_ptr<mapit::msgs::Entity> entity, std::shared_ptr<TfEntitydata> entityData, tf::TransformStamped tf)
+{
+    // safe header to entity
+    long sec, nsec;
+    mapit::time::to_sec_and_nsec( tf.stamp, sec, nsec);
+    mapit::msgs::Time *entity_stamp = new mapit::msgs::Time(); //entity->mutable_stamp();
+    entity_stamp->set_sec( sec );
+    entity_stamp->set_nsec( nsec );
+    entity->set_allocated_stamp( entity_stamp );
+
+    entity->set_frame_id(tf.frame_id);
+    env->getCheckout()->storeEntity(entity_name, entity);
+
+    // safe data to entitydata
+    upns::tf::TransformPtr tf_transform( new upns::tf::Transform(tf.transform) );
+    entityData->setData( tf_transform );
+}
+
+upns::StatusCode operate(upns::OperationEnvironment* env)
+{
+    /** structure:
+     * {
+     *  "map" : ...,
+     *  "dynamic_layer" : ...,  [optional]
+     *  "static_layer" : ...,   [optional]
+     *  "transforms" :
+     *  [                       [repeated]
+     *      {
+     *          "static" : ...,
+     *          "header" : {
+     *              "frame_id" : ...,
+     *              "stamp" : {
+     *                  "sec" : ...,
+     *                  "nsec" : ...
+     *              }
+     *          },
+     *
+     *          "transform" : {
+     *              "child_frame_id" : ...,
+     *              "translation" : {
+     *                  "x" : ...,
+     *                  "y" : ...,
+     *                  "z" : ...
+     *              },
+     *              "rotation" : {
+     *                  "w" : ...,
+     *                  "x" : ...,
+     *                  "y" : ...,
+     *                  "z" : ...
+     *              }
+     *          }
+     *      },
+     *      {
+     *          ...
+     *      }
+     *  ]
+     * }
+     */
+    QJsonDocument paramsDoc = QJsonDocument::fromJson( QByteArray(env->getParameters().c_str(), env->getParameters().length()) );
+    QJsonObject params(paramsDoc.object());
+
+    std::string layername_dynamic("tf_dynamic");
+    std::string layername_static("tf_static");
+    std::string map = params["map"].toString().toStdString();
+
+    // TODO check if data is available
+    QJsonArray json_transforms( params["transforms"].toArray() );
+    for ( QJsonValue json_value_tf : json_transforms ) {
+        QJsonObject json_tf( json_value_tf.toObject() );
+
+        bool layer_is_static = json_tf["static"].toBool();
+
+        // get data from json
+        tf::TransformStamped tf_loaded;
+
+        // get header
+        QJsonObject header( json_tf["header"].toObject() );
+        tf_loaded.frame_id = header["frame_id"].toString().toStdString();
+        QJsonObject stamp( header["stamp"].toObject() );
+        // TODO when int is not enough we can use string and cast this to long (jason does not support long)
+        tf_loaded.stamp = mapit::time::from_sec_and_nsec(stamp["sec"].toInt(), stamp["nsec"].toInt());
+
+        // get data
+        QJsonObject json_transform( json_tf["transform"].toObject() );
+        tf_loaded.transform.child_frame_id = json_transform["child_frame_id"].toString().toStdString();
+        QJsonObject json_translation( json_transform["translation"].toObject() );
+        QJsonObject json_rotation( json_transform["rotation"].toObject() );
+        tf_loaded.transform.translation = Eigen::Translation3f(
+                    (float)json_translation["x"].toDouble()
+                ,   (float)json_translation["y"].toDouble()
+                ,   (float)json_translation["z"].toDouble()
+                    );
+
+        tf_loaded.transform.rotation = Eigen::Quaternionf(
+                    (float)json_rotation["w"].toDouble()
+                ,   (float)json_rotation["x"].toDouble()
+                ,   (float)json_rotation["y"].toDouble()
+                ,   (float)json_rotation["z"].toDouble()
+                    );
+
+        // generate entity name for TF
+        std::string entity_name = "";
+        if (layer_is_static) {
+            entity_name = map + "/" + layername_static + "/" + tf_loaded.frame_id + std::to_string( mapit::time::to_sec(tf_loaded.stamp) ) + tf_loaded.transform.child_frame_id;
+        } else {
+            entity_name = map + "/" + layername_dynamic + "/" + tf_loaded.frame_id + std::to_string( mapit::time::to_sec(tf_loaded.stamp) ) + tf_loaded.transform.child_frame_id;
+        }
+
+        std::shared_ptr<mapit::msgs::Entity> entity;
+        std::shared_ptr<TfEntitydata> entityData;
+        upns::StatusCode status = getEntity(env, entity_name, entity, entityData);
+        if ( status != UPNS_STATUS_OK ) {
+            return status;
+        }
+
+        saveEntity(env, entity_name, entity, entityData, tf_loaded);
+    }
+
+    return UPNS_STATUS_OK;
+}
+
+UPNS_MODULE(OPERATOR_NAME, "store transforms in mapit", "fhac", OPERATOR_VERSION, "any", &operate)
