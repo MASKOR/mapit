0560f863143b45c4690766067f0ab45d1c230e12
Tobias Neumann <t.neumann@fh-aachen.de>
2017-10-24 16:12:50 +0200

    tools/ls: fixed listing of workspaces
    workspace was mandetory before, now this can be listed as well

--- tools/ls/src/main.cpp
+++ tools/ls/src/main.cpp
@@ -12,7 +12,7 @@ get_program_options(int argc, char *argv[], po::variables_map& vars)
   po::options_description program_options_desc(std::string("Usage: ") + argv[0] + " <workspace name>");
   program_options_desc.add_options()
       ("help,h", "print usage")
-      ("workspace,w", po::value<std::string>()->required(), "the workspace (formerly checkout) to work with")
+      ("workspace,w", po::value<std::string>(), "the workspace (formerly checkout) to work with")
       ("recursive,r", po::bool_switch()->default_value(false), "the structure should be displayed recursivly")
       ("map,m", po::value<std::string>(), "the content within the map")
       ("layer,l", po::value<std::string>(), "the content within the layer, needs map to be specified")
@@ -42,21 +42,27 @@ get_program_options(int argc, char *argv[], po::variables_map& vars)
 }
 
 void
-display_map(std::string name)
+display_checkout(std::string name)
 {
   log_info("- " + name);
 }
 
 void
+display_map(std::string name)
+{
+  log_info("  |- " + name);
+}
+
+void
 display_layer(std::string name, std::string type)
 {
-  log_info("  |- " + name + "\t\t[" + type + "]");
+  log_info("  |  |- " + name + "\t\t[" + type + "]");
 }
 
 void
 display_entity(std::string name)
 {
-  log_info("  |  |- " + name);
+  log_info("  |  |  |- " + name);
 }
 
 void
@@ -92,6 +98,19 @@ display_maps(std::shared_ptr<upns::Checkout> co, bool use_recursive)
   }
 }
 
+void
+display_checkouts(std::shared_ptr<upns::Repository> repo, bool use_recursive)
+{
+  std::vector<std::string> co_names = repo->listCheckoutNames();
+  for (auto co_name : co_names) {
+    display_checkout(co_name);
+    if (use_recursive) {
+      std::shared_ptr<upns::Checkout> co = repo->getCheckout( co_name );
+      display_maps(co, use_recursive);
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
     upns_init_logging();
@@ -104,7 +123,12 @@ int main(int argc, char *argv[])
       return ret;
     }
     bool use_recursive = vars["recursive"].as<bool>();
+    bool use_workspace = vars.count("workspace");
     bool use_map = vars.count("map");
+    if (use_map && ! use_workspace) {
+      log_error("when a map is given, a workspace needs to be given as well");
+      return 1;
+    }
     bool use_layer = vars.count("layer");
     if (use_layer && ! use_map) {
       log_error("when a layer is given, a map needs to be given as well");
@@ -118,39 +142,44 @@ int main(int argc, char *argv[])
 
     // connect to repo and workspace
     std::shared_ptr<upns::Repository> repo( upns::RepositoryFactoryStandard::openRepository( vars ) );
-    std::shared_ptr<upns::Checkout> co = repo->getCheckout( vars["workspace"].as<std::string>() );
-    if(co == nullptr)
-    {
-        log_error("Checkout \"" + vars["workspace"].as<std::string>() + "\" not found");
-        std::vector<std::string> possibleCheckouts = repo->listCheckoutNames();
-        if (possibleCheckouts.size() == 0) {
-            log_info("No possible checkout");
-        }
-        for ( std::string checkout : possibleCheckouts ) {
-            log_info("Possible checkout: " + checkout);
-        }
-        return 1;
-    }
+    if (use_workspace) {
+      std::shared_ptr<upns::Checkout> co = repo->getCheckout( vars["workspace"].as<std::string>() );
+      if(co == nullptr)
+      {
+          log_error("Checkout \"" + vars["workspace"].as<std::string>() + "\" not found");
+          std::vector<std::string> possibleCheckouts = repo->listCheckoutNames();
+          if (possibleCheckouts.size() == 0) {
+              log_info("No possible checkout");
+          }
+          for ( std::string checkout : possibleCheckouts ) {
+              log_info("Possible checkout: " + checkout);
+          }
+          return 1;
+      }
 
-    if (use_entity) {
-      std::shared_ptr<mapit::Map> map = co->getMap( vars["map"].as<std::string>() );
-      display_map(map->getName());
-      std::shared_ptr<mapit::Layer> layer = co->getLayer( map, vars["layer"].as<std::string>() );
-      display_layer(layer->getName(), layer->getTypeString());
-      std::shared_ptr<mapit::Entity> entity = co->getEntity( layer, vars["entity"].as<std::string>() );
-      display_entity(entity->getName());
-    } else if (use_layer) {
-      std::shared_ptr<mapit::Map> map = co->getMap( vars["map"].as<std::string>() );
-      display_map(map->getName());
-      std::shared_ptr<mapit::Layer> layer = co->getLayer( map, vars["layer"].as<std::string>() );
-      display_layer(layer->getName(), layer->getTypeString());
-      display_entities(co, layer, use_recursive);
-    } else if (use_map) {
-      std::shared_ptr<mapit::Map> map = co->getMap( vars["map"].as<std::string>() );
-      display_map(map->getName());
-      display_layers(co, map, use_recursive);
-    } else if ( ! use_map ) {
-      display_maps(co, use_recursive);
+      display_checkout( vars["workspace"].as<std::string>() );
+      if (use_entity) {
+        std::shared_ptr<mapit::Map> map = co->getMap( vars["map"].as<std::string>() );
+        display_map(map->getName());
+        std::shared_ptr<mapit::Layer> layer = co->getLayer( map, vars["layer"].as<std::string>() );
+        display_layer(layer->getName(), layer->getTypeString());
+        std::shared_ptr<mapit::Entity> entity = co->getEntity( layer, vars["entity"].as<std::string>() );
+        display_entity(entity->getName());
+      } else if (use_layer) {
+        std::shared_ptr<mapit::Map> map = co->getMap( vars["map"].as<std::string>() );
+        display_map(map->getName());
+        std::shared_ptr<mapit::Layer> layer = co->getLayer( map, vars["layer"].as<std::string>() );
+        display_layer(layer->getName(), layer->getTypeString());
+        display_entities(co, layer, use_recursive);
+      } else if (use_map) {
+        std::shared_ptr<mapit::Map> map = co->getMap( vars["map"].as<std::string>() );
+        display_map(map->getName());
+        display_layers(co, map, use_recursive);
+      } else if ( ! use_map ) {
+        display_maps(co, use_recursive);
+      }
+    } else {
+      display_checkouts(repo, use_recursive);
     }
 
     return 0;
