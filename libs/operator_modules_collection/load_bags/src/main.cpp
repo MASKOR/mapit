4ac2eff8906ace6c51b1586b6e62cb308b3ff5aa
Tobias Neumann <t.neumann@fh-aachen.de>
2017-10-24 22:39:29 +0200

    operator/load_bags: add tfs to be loaded

--- libs/operator_modules_collection/load_bags/src/main.cpp
+++ libs/operator_modules_collection/load_bags/src/main.cpp
@@ -31,6 +31,7 @@ upns::StatusCode operate(upns::OperationEnvironment* env)
      *          "topic" : ...,
      *          "layer" : ...,
      *          "type" : ...,   [pointcloud, tf, TODO]
+//     *          "is_static" : ..., [only for tf]
      *      },
      *      {
      *          ...
@@ -84,20 +85,44 @@ upns::StatusCode operate(upns::OperationEnvironment* env)
             if (pc2 != NULL) {
               std::string entity_name = pc2->header.frame_id + std::to_string(pc2->header.stamp.sec) + "." + std::to_string(pc2->header.stamp.nsec);
               std::shared_ptr<mapit::Entity> entity = checkout->getExistingOrNewEntity(layer, entity_name);
+              entity->set_frame_id( pc2->header.frame_id );
+              entity->set_stamp( mapit::time::from_sec_and_nsec( pc2->header.stamp.sec, pc2->header.stamp.nsec ));
 
               std::shared_ptr<pcl::PCLPointCloud2> entity_data = std::make_shared<pcl::PCLPointCloud2>();
               pcl_conversions::toPCL(*pc2, *entity_data);
 
-              entity->set_frame_id( pc2->header.frame_id );
-              entity->set_stamp( mapit::time::from_sec_and_nsec( pc2->header.stamp.sec, pc2->header.stamp.nsec ));
-
+              log_info("  entity: " + entity_name);
               checkout->storeEntity(entity, entity_data);
             } else {
               log_error("Data [" + bag_name + "] : \"" + topic_name + "\" is not of type \"sensor_msgs::PointCloud2\"");
               return UPNS_STATUS_ERROR;
             }
           } else if ( 0 == type_name.compare("tf") ) {
+            tf2_msgs::TFMessage::ConstPtr tf2 = msg.instantiate<tf2_msgs::TFMessage>();
+            if (tf2 != NULL) {
+              for (geometry_msgs::TransformStamped ros_tf : tf2->transforms) {
+                std::string entity_name = ros_tf.header.frame_id + std::to_string(ros_tf.header.stamp.sec) + "." + std::to_string(ros_tf.header.stamp.nsec) + ros_tf.child_frame_id;
+                std::shared_ptr<mapit::Entity> entity = checkout->getExistingOrNewEntity(layer, entity_name);
+                entity->set_frame_id( ros_tf.header.frame_id );
+                entity->set_stamp( mapit::time::from_sec_and_nsec( ros_tf.header.stamp.sec, ros_tf.header.stamp.nsec ) );
 
+                std::shared_ptr<tf::Transform> entity_data = std::make_shared<tf::Transform>();
+
+                entity_data->child_frame_id = ros_tf.child_frame_id;
+                entity_data->translation.x() = ros_tf.transform.translation.x;
+                entity_data->translation.y() = ros_tf.transform.translation.y;
+                entity_data->translation.z() = ros_tf.transform.translation.z;
+                entity_data->rotation.w() = ros_tf.transform.rotation.w;
+                entity_data->rotation.x() = ros_tf.transform.rotation.x;
+                entity_data->rotation.y() = ros_tf.transform.rotation.y;
+                entity_data->rotation.z() = ros_tf.transform.rotation.z;
+
+                checkout->storeEntity(entity, entity_data);
+              }
+            } else {
+              log_error("Data [" + bag_name + "] : \"" + topic_name + "\" is not of type \"tf2_msgs::TFMessage\"");
+              return UPNS_STATUS_ERROR;
+            }
           }
         }
       }
