e8f614d109534ea2b28b368c824c68d642a7a0c1
Tobias Neumann <t.neumann@fh-aachen.de>
2017-11-15 16:17:10 +0100

    refactoring: use static_pointer_cast instread of dynamic

--- tools/stream_to_ros/src/publishpointclouds.cpp
+++ tools/stream_to_ros/src/publishpointclouds.cpp
@@ -8,9 +8,14 @@ void
 PublishPointClouds::publish_entity(std::shared_ptr<mapit::Entity> entity)
 {
   // get data
-  std::shared_ptr<upns::AbstractEntitydata> entity_data_abstract = checkout_->getEntityDataReadOnly(entity);
+  std::shared_ptr<upns::AbstractEntitydata> entity_data_abstract = checkout_->getEntitydataReadOnly(entity);
+  if ( 0 != std::strcmp(entity_data_abstract->type(), PointcloudEntitydata::TYPENAME()) ) {
+    log_error("stream_to_ros: can't publish " + entity->getName() + " since the type is wrongly given\n"
+              "type is: " + entity_data_abstract->type() + " but we need: " + PointcloudEntitydata::TYPENAME());
+    return;
+  }
   std::shared_ptr<pcl::PCLPointCloud2> entity_data =
-      std::dynamic_pointer_cast<PointcloudEntitydata>(
+      std::static_pointer_cast<PointcloudEntitydata>(
         entity_data_abstract
         )->getData();
 
