4ddf384d57defa4efc8fde9cd6b0c6cd2c0d9968
Tobias Neumann <t.neumann@fh-aachen.de>
2017-12-07 17:23:49 +0100

    operator/reg_local_icp: execute ICP on more than one input

--- libs/operator_modules_collection/reg_local_icp/src/icp.cpp
+++ libs/operator_modules_collection/reg_local_icp/src/icp.cpp
@@ -88,7 +88,9 @@ mapit::ICP::get_pointcloud(std::string path, upns::StatusCode &status, mapit::ti
       return nullptr;
     }
     std::string frame_id = entity->frame_id();
-    stamp = mapit::time::from_sec_and_nsec( entity->stamp().sec(), entity->stamp().nsec() );
+    unsigned long sec = entity->stamp().sec();
+    unsigned long nsec = entity->stamp().nsec();
+    stamp = mapit::time::from_sec_and_nsec( sec, nsec );
     std::shared_ptr<AbstractEntitydata> abstract_entitydata = checkout_->getEntitydataForReadWrite( path );
     if ( 0 != std::strcmp( abstract_entitydata->type(), PointcloudEntitydata::TYPENAME() )) {
       status = UPNS_STATUS_INVALID_ARGUMENT;
@@ -113,111 +115,121 @@ mapit::ICP::get_pointcloud(std::string path, upns::StatusCode &status, mapit::ti
 upns::StatusCode
 mapit::ICP::operate()
 {
-    // get input clouds
-    upns::StatusCode status;
-    mapit::time::Stamp input_stamp;
-    pcl::PCLHeader input_header;
-    std::shared_ptr<PointcloudEntitydata> entitydata_input;
-    boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> input_pc_transformed = get_pointcloud( cfg_input_.front(), status, input_stamp, input_header, entitydata_input );
-    if ( ! upnsIsOk(status) ) {
-        return status;
-    }
+    for (std::string cfg_input_one : cfg_input_) {
+        // get input clouds
+        upns::StatusCode status;
+        mapit::time::Stamp input_stamp;
+        pcl::PCLHeader input_header;
+        std::shared_ptr<PointcloudEntitydata> entitydata_input;
+        boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> input_pc_transformed = get_pointcloud( cfg_input_one, status, input_stamp, input_header, entitydata_input );
+        if ( ! upnsIsOk(status) ) {
+            return status;
+        }
 
-    // get target cloud
-    mapit::time::Stamp target_stamp;
-    pcl::PCLHeader target_header;
-    std::shared_ptr<PointcloudEntitydata> entitydata_target;
-    boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> target_pc_transformed = get_pointcloud( cfg_target_, status, target_stamp, target_header, entitydata_target );
-    if ( ! upnsIsOk(status) ) {
-        return status;
-    }
+        // get target cloud
+        mapit::time::Stamp target_stamp;
+        pcl::PCLHeader target_header;
+        std::shared_ptr<PointcloudEntitydata> entitydata_target;
+        boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>> target_pc_transformed = get_pointcloud( cfg_target_, status, target_stamp, target_header, entitydata_target );
+        if ( ! upnsIsOk(status) ) {
+            return status;
+        }
 
-    // do ICP
-    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-    icp.setInputSource(input_pc_transformed);
-    icp.setInputTarget(target_pc_transformed);
-    pcl::PointCloud<pcl::PointXYZ> icp_out;
-    icp.align(icp_out);
+        // do ICP
+        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+        icp.setInputSource(input_pc_transformed);
+        icp.setInputTarget(target_pc_transformed);
+        pcl::PointCloud<pcl::PointXYZ> icp_out;
+        icp.align(icp_out);
 
-    if ( ! icp.hasConverged() ) {
-      log_error("reg_local_icp: algorithm didn't converged");
-      return UPNS_STATUS_ERROR;
-    }
+        if ( ! icp.hasConverged() ) {
+          log_error("reg_local_icp: algorithm didn't converged");
+          return UPNS_STATUS_ERROR;
+        } else {
+            log_info("reg_local_icp: ICP for cloud \"" + cfg_input_one + "\" to \"" + cfg_target_
+                   + "\" finished with fitness score " + std::to_string(icp.getFitnessScore()));
+        }
 
-    Eigen::Affine3f transform( icp.getFinalTransformation() );
-    log_info("reg_local_icp: finished");
-    std::cout << transform.matrix() << std::endl;
+        Eigen::Affine3f transform( icp.getFinalTransformation() );
 
-    // handle the result
-    switch (cfg_handle_result_) {
-      case HandleResult::data_change: {
-        log_info("reg_local_icp: change data");
-        log_warn("reg_local_icp: only XYZ will survive, intensity and color will be lost");
-        // TODO find way to transform pointcloud2 so that all data survive
-        std::shared_ptr<pcl::PCLPointCloud2> icp_out2 = std::make_shared<pcl::PCLPointCloud2>();
-        pcl::toPCLPointCloud2(icp_out, *icp_out2);
-        icp_out2->header = input_header;
-        entitydata_input->setData(icp_out2);
-        break;
-      }
-      case HandleResult::tf_add: {
-        log_info("reg_local_icp: add tf");
-        // get infos
-        mapit::time::Stamp stamp = input_stamp;
-        std::string entity_name = cfg_tf_prefix_ + "/" + upns::tf::store::TransformStampedList::get_entity_name(cfg_tf_frame_id_, cfg_tf_child_frame_id_);
-        // get entity and data
-        // TODO how to check for existance of entity?
-        std::shared_ptr<mapit::msgs::Entity> entity = checkout_->getEntity( entity_name );
-        std::shared_ptr<TfEntitydata> ed_tf;
-        if (entity == nullptr) {
-          entity = std::make_shared<mapit::msgs::Entity>();
-          entity->set_type(TfEntitydata::TYPENAME());
-          StatusCode s = checkout_->storeEntity(entity_name, entity);
-          if( ! upnsIsOk(s) ) {
-            log_error("reg_local_icp: Failed to create entity.");
-            return UPNS_STATUS_ERROR;
+        // handle the result
+        switch (cfg_handle_result_) {
+          case HandleResult::data_change: {
+            log_info("reg_local_icp: change pointcloud " + cfg_input_one + " with tf:");
+            std::cout << transform.matrix() << std::endl;
+            log_warn("reg_local_icp: only XYZ will survive, intensity and color will be lost");
+            // TODO find way to transform pointcloud2 so that all data survive
+            std::shared_ptr<pcl::PCLPointCloud2> icp_out2 = std::make_shared<pcl::PCLPointCloud2>();
+            pcl::toPCLPointCloud2(icp_out, *icp_out2);
+            icp_out2->header = input_header;
+            entitydata_input->setData(icp_out2);
+            break;
+          }
+          case HandleResult::tf_add: {
+            unsigned long sec_log, nsec_log;
+            mapit::time::to_sec_and_nsec(input_stamp, sec_log, nsec_log);
+            log_info("reg_local_icp: add "
+                   + (cfg_tf_is_static_ ? "static" : "dynamic")
+                   + " transform from \"" + cfg_tf_frame_id_ + "\" to \"" + cfg_tf_child_frame_id_
+                   + "\" at time " + std::to_string(sec_log) + "." + std::to_string(nsec_log) );
+            std::cout << transform.matrix() << std::endl;
+            // get infos
+            mapit::time::Stamp stamp = input_stamp;
+            std::string entity_name = cfg_tf_prefix_ + "/" + upns::tf::store::TransformStampedList::get_entity_name(cfg_tf_frame_id_, cfg_tf_child_frame_id_);
+            // get entity and data
+            // TODO how to check for existance of entity?
+            std::shared_ptr<mapit::msgs::Entity> entity = checkout_->getEntity( entity_name );
+            std::shared_ptr<TfEntitydata> ed_tf;
+            if (entity == nullptr) {
+              entity = std::make_shared<mapit::msgs::Entity>();
+              entity->set_type(TfEntitydata::TYPENAME());
+              StatusCode s = checkout_->storeEntity(entity_name, entity);
+              if( ! upnsIsOk(s) ) {
+                log_error("reg_local_icp: Failed to create entity.");
+                return UPNS_STATUS_ERROR;
+              }
+            }
+            std::shared_ptr<upns::AbstractEntitydata> ed_a = checkout_->getEntitydataForReadWrite(entity_name);
+            if ( 0 != std::strcmp(ed_a->type(), TfEntitydata::TYPENAME()) ) {
+              log_error("reg_local_icp: can't add tf, retrieved entity is not of type TfEntitydata");
+              return UPNS_STATUS_ERROR;
+            }
+            ed_tf = std::static_pointer_cast<TfEntitydata>(ed_a);
+            std::shared_ptr<tf::store::TransformStampedList> ed_d = ed_tf->getData();
+            if (ed_d == nullptr) {
+                ed_d = std::make_shared<tf::store::TransformStampedList>(cfg_tf_frame_id_, cfg_tf_child_frame_id_, cfg_tf_is_static_);
+            }
+
+            std::unique_ptr<upns::tf::TransformStamped> tfs = std::make_unique<upns::tf::TransformStamped>();
+
+            // add data
+            tfs->frame_id = cfg_tf_frame_id_;
+            tfs->child_frame_id = cfg_tf_child_frame_id_;
+            tfs->stamp = stamp;
+            tfs->transform.translation.translation() = transform.translation();
+            tfs->transform.rotation = transform.rotation();
+            ed_d->add_TransformStamped( std::move(tfs), cfg_tf_is_static_);
+
+            unsigned long sec, nsec;
+            mapit::time::to_sec_and_nsec(stamp, sec, nsec);
+            entity->set_frame_id( cfg_tf_frame_id_ );
+            entity->mutable_stamp()->set_sec( sec );
+            entity->mutable_stamp()->set_nsec( nsec );
+
+            // write data
+            checkout_->storeEntity(entity_name, entity);
+            ed_tf->setData(ed_d);
+            break;
+          }
+          case HandleResult::tf_change: {
+            log_info("reg_local_icp: change tf");
+            log_error("reg_local_icp: do not handle result of ICP (no effect), its not yet implemented.");
+            break;
+          }
+          default: {
+            log_error("reg_local_icp: do not handle result of ICP (no effect), its not yet implemented.");
           }
         }
-        std::shared_ptr<upns::AbstractEntitydata> ed_a = checkout_->getEntitydataForReadWrite(entity_name);
-        if ( 0 != std::strcmp(ed_a->type(), TfEntitydata::TYPENAME()) ) {
-          log_error("reg_local_icp: can't add tf, retrieved entity is not of type TfEntitydata");
-          return UPNS_STATUS_ERROR;
-        }
-        ed_tf = std::static_pointer_cast<TfEntitydata>(ed_a);
-        std::shared_ptr<tf::store::TransformStampedList> ed_d = ed_tf->getData();
-        if (ed_d == nullptr) {
-            ed_d = std::make_shared<tf::store::TransformStampedList>(cfg_tf_frame_id_, cfg_tf_child_frame_id_, false);
-        }
-
-        std::unique_ptr<upns::tf::TransformStamped> tfs = std::make_unique<upns::tf::TransformStamped>();
-
-        // add data
-        tfs->frame_id = cfg_tf_frame_id_;
-        tfs->child_frame_id = cfg_tf_child_frame_id_;
-        tfs->stamp = stamp;
-        tfs->transform.translation.translation() = transform.translation();
-        tfs->transform.rotation = transform.rotation();
-        ed_d->add_TransformStamped( std::move(tfs), false);
-
-        unsigned long sec, nsec;
-        mapit::time::to_sec_and_nsec(stamp, sec, nsec);
-        entity->set_frame_id( cfg_tf_frame_id_ );
-        entity->mutable_stamp()->set_sec( sec );
-        entity->mutable_stamp()->set_nsec( nsec );
-
-        // write data
-        checkout_->storeEntity(entity_name, entity);
-        ed_tf->setData(ed_d);
-        break;
-      }
-      case HandleResult::tf_change: {
-        log_info("reg_local_icp: change tf");
-        log_error("reg_local_icp: do not handle result of ICP (no effect), its not yet implemented.");
-        break;
-      }
-      default: {
-        log_error("reg_local_icp: do not handle result of ICP (no effect), its not yet implemented.");
-      }
     }
 
     return UPNS_STATUS_OK;
