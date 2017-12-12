ed94a97950be024bb5bf280cfcaeb0a3829ecf05
Tobias Neumann <t.neumann@fh-aachen.de>
2017-12-12 16:40:13 +0100

    operator/load_bags: remove map/layer/entity

--- libs/operator_modules_collection/load_bags/src/main.cpp
+++ libs/operator_modules_collection/load_bags/src/main.cpp
@@ -12,6 +12,8 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 
+#include <mapit/time/time.h>
+
 #include <tf2_msgs/TFMessage.h>
 #include <upns/layertypes/tflayer.h>
 
@@ -19,19 +21,35 @@
 #include <upns/layertypes/pointcloudlayer.h>
 #include <pcl_conversions/pcl_conversions.h>
 
-upns::StatusCode operate(upns::OperationEnvironment* env)
+upns::StatusCode get_or_create_entity(  CheckoutRaw* checkout
+                                      , const std::string& entity_name
+                                      , const std::string& entity_type
+                                      , std::shared_ptr<mapit::msgs::Entity>& entity)
+{
+    entity = checkout->getEntity(entity_name);
+    if (entity == nullptr) {
+        entity = std::make_shared<mapit::msgs::Entity>();
+        entity->set_type( entity_type );
+        checkout->storeEntity(entity_name, entity);
+    }
+    if ( 0 == entity->type().compare( entity_type ) ) {
+        return UPNS_STATUS_OK;
+    } else {
+        return UPNS_STATUS_ERROR;
+    }
+}
+
+upns::StatusCode operate_load_bags(upns::OperationEnvironment* env)
 {
     /** structure:
      * {
-     *  "map" : ...,
      *  "bags" : ["name_of_bag_1", ...],
      *  "translation_pairs" :
      *  [                       [repeated]
      *      {
      *          "topic" : ...,
-     *          "layer" : ...,
+     *          "prefix" : ...,
      *          "type" : ...,   [pointcloud, tf, TODO]
-//     *          "is_static" : ..., [only for tf]
      *      },
      *      {
      *          ...
@@ -42,16 +60,13 @@ upns::StatusCode operate(upns::OperationEnvironment* env)
     QJsonDocument paramsDoc = QJsonDocument::fromJson( QByteArray(env->getParameters().c_str(), env->getParameters().length()) );
     QJsonObject params(paramsDoc.object());
 
-    std::string map_name = params["map"].toString().toStdString();
-
     CheckoutRaw* checkout = env->getCheckout();
-    std::shared_ptr<mapit::Map> map = checkout->getExistingOrNewMap(map_name);
 
     QJsonArray json_bag_names( params["bags"].toArray() );
     for (auto json_bag_name : json_bag_names) {
       std::string bag_name = json_bag_name.toString().toStdString();
 
-      log_info("Open bag " + bag_name);
+      log_info("load_bags: Open bag " + bag_name);
       rosbag::Bag bag;
       bag.open(bag_name, rosbag::bagmode::Read);
 
@@ -59,10 +74,8 @@ upns::StatusCode operate(upns::OperationEnvironment* env)
       for (auto json_translation_pair : json_translation_pairs) {
         QJsonObject json_translation_pair_obj( json_translation_pair.toObject() );
         std::string topic_name = json_translation_pair_obj["topic"].toString().toStdString();
-        std::string layer_name = json_translation_pair_obj["layer"].toString().toStdString();
+        std::string prefix_name = json_translation_pair_obj["prefix"].toString().toStdString() + "/";
         std::string type_name = json_translation_pair_obj["type"].toString().toStdString();
-
-        log_info("Import [" + bag_name + "] : \"" + topic_name + "\"\t=> \"" + map_name + "/" + layer_name + "\"\t(type: " + type_name + ")");
 
         std::string mapit_type = "";
         if (        0 == type_name.compare("pointcloud") ) {
@@ -70,10 +83,11 @@ upns::StatusCode operate(upns::OperationEnvironment* env)
         } else if ( 0 == type_name.compare("tf") ) {
           mapit_type = TfEntitydata::TYPENAME();
         } else {
-          log_error("unknown type specified");
+          log_error("load_bags: unknown type specified");
           return UPNS_STATUS_ERROR;
         }
-        std::shared_ptr<mapit::Layer> layer = checkout->getExistingOrNewLayer(map, layer_name, mapit_type);
+
+        log_info("load_bags: Import [" + bag_name + "] : \"" + topic_name + "\"\t=> \"" + prefix_name + "/*\"\t(type: " + mapit_type + ")");
 
         std::vector<std::string> topics;
         topics.push_back( topic_name );
@@ -84,30 +98,37 @@ upns::StatusCode operate(upns::OperationEnvironment* env)
 
         // for all msgs
         for (const rosbag::MessageInstance msg : view) {
-          if (        0 == type_name.compare("pointcloud") ) {
+          if (        0 == mapit_type.compare( PointcloudEntitydata::TYPENAME() ) ) {
             sensor_msgs::PointCloud2::ConstPtr pc2 = msg.instantiate<sensor_msgs::PointCloud2>();
             if (pc2 != NULL) {
-              std::string entity_name = pc2->header.frame_id + std::to_string(pc2->header.stamp.sec) + "." + std::to_string(pc2->header.stamp.nsec);
+              std::string entity_name = prefix_name + "/" + pc2->header.frame_id + std::to_string(pc2->header.stamp.sec) + "." + std::to_string(pc2->header.stamp.nsec);
 
-              std::shared_ptr<mapit::Entity> entity = checkout->getExistingOrNewEntity(layer, entity_name);
+              std::shared_ptr<mapit::msgs::Entity> entity;
+              upns::StatusCode get_entity_status = get_or_create_entity(checkout, entity_name, PointcloudEntitydata::TYPENAME(), entity);
+              if ( ! upnsIsOk(get_entity_status) ) {
+                  log_error("load_bags: Can't get entity \"" + entity_name + "\" of type \"" + PointcloudEntitydata::TYPENAME() + "\"");
+                  return get_entity_status;
+              }
               entity->set_frame_id( pc2->header.frame_id );
-              entity->set_stamp( mapit::time::from_sec_and_nsec( pc2->header.stamp.sec, pc2->header.stamp.nsec ));
+              entity->mutable_stamp()->set_sec(pc2->header.stamp.sec);
+              entity->mutable_stamp()->set_nsec(pc2->header.stamp.nsec);
 
               std::shared_ptr<pcl::PCLPointCloud2> entity_data = std::make_shared<pcl::PCLPointCloud2>();
               pcl_conversions::toPCL(*pc2, *entity_data);
 
-              log_info("  entity: " + entity_name);
-              checkout->storeEntity(entity, entity_data);
+              log_info("load_bags:   entity: " + entity_name);
+              checkout->storeEntity(entity_name, entity);
+              std::static_pointer_cast<PointcloudEntitydata>(checkout->getEntitydataForReadWrite(entity_name))->setData(entity_data);
             } else {
-              log_error("Data [" + bag_name + "] : \"" + topic_name + "\" is not of type \"sensor_msgs::PointCloud2\"");
+              log_error("load_bags: Data [" + bag_name + "] : \"" + topic_name + "\" is not of type \"sensor_msgs::PointCloud2\"");
               return UPNS_STATUS_ERROR;
             }
-          } else if ( 0 == type_name.compare("tf") ) {
+          } else if ( 0 == mapit_type.compare( TfEntitydata::TYPENAME() ) ) {
             tf2_msgs::TFMessage::ConstPtr tf2 = msg.instantiate<tf2_msgs::TFMessage>();
             if (tf2 != NULL) {
 
               for (geometry_msgs::TransformStamped ros_tf : tf2->transforms) {
-                std::unique_ptr<tf::TransformStamped> tf_loaded = std::unique_ptr<tf::TransformStamped>(new tf::TransformStamped);
+                std::unique_ptr<tf::TransformStamped> tf_loaded = std::make_unique<tf::TransformStamped>();
 
                 tf_loaded->frame_id = ros_tf.header.frame_id;
                 tf_loaded->stamp = mapit::time::from_sec_and_nsec( ros_tf.header.stamp.sec, ros_tf.header.stamp.nsec );
@@ -129,16 +150,16 @@ upns::StatusCode operate(upns::OperationEnvironment* env)
                 tfs_map.add_transform( std::move( tf_loaded ), is_static );
               }
             } else {
-              log_error("Data [" + bag_name + "] : \"" + topic_name + "\" is not of type \"tf2_msgs::TFMessage\"");
+              log_error("load_bags: Data [" + bag_name + "] : \"" + topic_name + "\" is not of type \"tf2_msgs::TFMessage\"");
               return UPNS_STATUS_ERROR;
             }
           }
         }
-        tfs_map.store_entities( checkout, layer );
+        tfs_map.store_entities( checkout, prefix_name );
       }
     }
 
     return UPNS_STATUS_OK;
 }
 
-UPNS_MODULE(OPERATOR_NAME, "store transforms in mapit", "fhac", OPERATOR_VERSION, "any", &operate)
+UPNS_MODULE(OPERATOR_NAME, "load bagfiles in mapit", "fhac", OPERATOR_VERSION, "any", &operate_load_bags)
