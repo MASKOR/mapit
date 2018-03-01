44eb57a39f14f0ec7605dbfbb7f6e88fd511f3fe
Tobias Neumann <t.neumann@fh-aachen.de>
2018-03-01 17:16:28 +0100

    licence: update script to work with cmake

--- script/add-header.py
+++ script/add-header.py
@@ -6,14 +6,24 @@ from collections import namedtuple
 if len(sys.argv) < 2:
   sys.exit("No dir given")
 
-proc_find = subprocess.Popen(["find " + sys.argv[1] + " \( -name \*.h -o -name \*.cpp -o -name \*.hpp -o -name \*.c -o -name \*.cxx -o -name \*.cc -o -name \*.c \)"], stdout=subprocess.PIPE, shell=True)
+use_cmake = False
+if len(sys.argv) >= 3:
+  if sys.argv[2] == 'cmake':
+    use_cmake = True
+
+proc_find = ""
+if use_cmake:
+  proc_find = subprocess.Popen(["find " + sys.argv[1] + " \( -name CMakeLists.txt \)"], stdout=subprocess.PIPE, shell=True)
+else:
+  proc_find = subprocess.Popen(["find " + sys.argv[1] + " \( -name \*.h -o -name \*.cpp -o -name \*.hpp -o -name \*.c -o -name \*.cxx -o -name \*.cc -o -name \*.c \)"], stdout=subprocess.PIPE, shell=True)
 (find, err) = proc_find.communicate()
 
 for file_name in find.splitlines():
-#  proc_cat = subprocess.Popen(["cat " + file_name], stdout=subprocess.PIPE, shell=True)
-#  (cat, err) = proc_cat.communicate()
-#  
-  proc_header = subprocess.Popen(["python generate-licence-header.py " + file_name], stdout=subprocess.PIPE, shell=True)
+  proc_header = ""
+  if use_cmake:
+    proc_header = subprocess.Popen(["python generate-licence-header.py " + file_name + " cmake"], stdout=subprocess.PIPE, shell=True)
+  else:
+    proc_header = subprocess.Popen(["python generate-licence-header.py " + file_name], stdout=subprocess.PIPE, shell=True)
   (header, err) = proc_header.communicate()
 
   with open(file_name, 'r+') as f:
--- script/generate-licence-header.py
+++ script/generate-licence-header.py
@@ -6,6 +6,11 @@ from collections import namedtuple
 if len(sys.argv) < 2:
   sys.exit("No file to check given")
 
+use_cmake = False
+if len(sys.argv) >= 3:
+  if sys.argv[2] == 'cmake':
+    use_cmake = True
+
 proc = subprocess.Popen(["git log --follow --date=short --pretty='%ad %an <%ae>' -- " + sys.argv[1] + " | sed 's/dbulla.*/Daniel Bulla <d.bulla@fh-aachen.de>/'"], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 #print "program output:", out
@@ -35,20 +40,51 @@ for line in outr.splitlines():
   if new_author:
     authors.append(Author(name, email, year, year))
 
-output = "/*******************************************************************************\n *\n"
+output = ""
+if use_cmake:
+  output = "################################################################################\n#\n"
+else:
+  output = "/*******************************************************************************\n *\n"
+
 first = True
 for a in authors:
   if first:
     first = False
-    output += " * Copyright "
+    if use_cmake:
+      output += "# Copyright "
+    else:
+      output += " * Copyright "
   else:
-    output += " *           "
+    if use_cmake:
+      output += "#           "
+    else:
+      output += " *           "
   if a.yearf == a.yeart:
     output += "     " + a.yeart
   else:
     output += a.yearf + "-" + a.yeart
   output += " " + a.name + "\t" + a.email + "\n"
-output += """ *
+if use_cmake:
+  output += """#
+################################################################################
+
+#  This file is part of mapit.
+#
+#  Mapit is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Mapit is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with mapit.  If not, see <http://www.gnu.org/licenses/>.
+#"""
+else:
+  output += """ *
 ******************************************************************************/
 
 /*  This file is part of mapit.
