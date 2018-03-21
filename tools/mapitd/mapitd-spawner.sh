63e1b9a0ff4f15fea39bcb3b0c16968abc9d6f5e
Daniel Bulla <d.bulla@fh-aachen.de>
2018-03-21 10:14:40 +0100

    mapitd: corrected script name and added symlinks

--- tools/mapitd/mapitd-spawner.sh
+++ tools/mapitd/mapitd-spawner.sh
@@ -6,5 +6,5 @@ echo "Spawning $1 processes"
 for ((i=0; i<$1; i++)); do 
     the_port=$(($2 + $i))
     echo "starting mapitd on port $the_port"
-    ( ./loop_single_server.sh --port $the_port "${@:3}" & )
+    ( ./mapitd-failsafe --port $the_port "${@:3}" & )
 done
