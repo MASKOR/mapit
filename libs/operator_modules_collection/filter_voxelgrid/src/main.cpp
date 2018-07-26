99bbc641b728b30f83098c157a6cf0f0da4d2a22
Tobias Neumann <t.neumann@fh-aachen.de>
2018-07-26 17:36:44 +0200

    operator/filter_voxelgrid: speedup concatination of PCLPointCloud2
    when two PCLPointCloud2 are concatenated, the whole memory is copied.
    When you have an operation like this where you have a growing
    PCLPointCloud2 where a new snipped is added sequently this slows down
    enormously, since the growing point cloud is copied over and over.
    
    Therfore this operator now adds 100 (I know static value is not the
    best) clouds into one "sub cloud", which is an intermediate cloud that
    stores the voxelgrid-result of up to 100 octree-leafs. Then the next
    "sub cloud" is started.
    Afterwards these "sub clouds" are merged in an binary fashion. This
    results in always two clouds of about the same sice been merged, and
    reduces the amount of copied data.

--- libs/operator_modules_collection/filter_voxelgrid/src/main.cpp
+++ libs/operator_modules_collection/filter_voxelgrid/src/main.cpp
@@ -49,7 +49,6 @@ mapit::StatusCode executeVoxelgrid(mapit::OperationEnvironment* env, const std::
         return MAPIT_STATUS_ERR_DB_INVALID_ARGUMENT;
     }
     mapit::entitytypes::Pointcloud2Ptr pc2 = entityData->getData();
-    mapit::entitytypes::Pointcloud2Ptr cloud_filtered = std::make_shared<pcl::PCLPointCloud2>();
 
     typedef pcl::PointXYZ Point;
     typedef pcl::PointCloud<Point> Cloud;
@@ -57,24 +56,31 @@ mapit::StatusCode executeVoxelgrid(mapit::OperationEnvironment* env, const std::
     boost::shared_ptr<Cloud> pc_octree_in = boost::make_shared<Cloud>();
     pcl::fromPCLPointCloud2(*pc2, *pc_octree_in);
 
-    double bounding_box_size = int( octreeLeafSize / leafSize + 1) * leafSize;
+    double bounding_box_size = leafSize * int( octreeLeafSize / leafSize + 1);
 
     // use octree to apply this filter on sub-clouds (one cloud is too big)
     pcl::octree::OctreePointCloudPointVector<Point> octree_for_bounding_box_of_sub_clouds(bounding_box_size);
     octree_for_bounding_box_of_sub_clouds.setInputCloud( pc_octree_in );
     octree_for_bounding_box_of_sub_clouds.addPointsFromInputCloud();
 
+    size_t number_of_leafs = octree_for_bounding_box_of_sub_clouds.getLeafCount();
+
+    // concatenate the clouds into a few clouds before these are concatenated togehter, for speedup
+    size_t number_of_clouds_to_be_summed_into_one = 100;
+    size_t number_of_sum_clouds = size_t(number_of_leafs / number_of_clouds_to_be_summed_into_one) + 1;
+    std::vector<std::shared_ptr<pcl::PCLPointCloud2>> clouds_filtered;
+    for (size_t i = 0; i < number_of_sum_clouds; ++i) {
+        clouds_filtered.push_back( std::make_shared<pcl::PCLPointCloud2>() );
+    }
+
+    log_info("Voxelgrid: process " + std::to_string(number_of_leafs) + " leafs of octree; and store into " + std::to_string(number_of_sum_clouds) + " \"sum clouds\"");
+
+    size_t count = 0;
     // for each sub-cloud
-    size_t count_total = octree_for_bounding_box_of_sub_clouds.getLeafCount();
-    log_info("Voxelgrid: process " + std::to_string(count_total) + " leafs of octree");
-    size_t count = 1;
     for ( pcl::octree::OctreePointCloud<Point>::LeafNodeIterator it = octree_for_bounding_box_of_sub_clouds.leaf_begin();
           it != octree_for_bounding_box_of_sub_clouds.leaf_end();
-          it++ ) {
-      if (count % 10 == 0) {
-          log_info("Voxelgrid: in leaf " + std::to_string(count) + "/" + std::to_string(count_total));
-      }
-      ++count;
+          it++, ++count ) {
+
       std::vector<int> indices_octree_leaf;
       it.getLeafContainer().getPointIndices(indices_octree_leaf);
 
@@ -89,14 +95,40 @@ mapit::StatusCode executeVoxelgrid(mapit::OperationEnvironment* env, const std::
       vg.filter(pc_voxelgrid_cloud);
 
       // add to final cloud
-      std::shared_ptr<pcl::PCLPointCloud2> cloud_filtered_new = std::make_shared<pcl::PCLPointCloud2>();
-      pcl::concatenatePointCloud(*cloud_filtered, pc_voxelgrid_cloud, *cloud_filtered_new);
-      cloud_filtered = cloud_filtered_new;
+      if (count % number_of_clouds_to_be_summed_into_one == 0) {
+          log_info("Voxelgrid: in leaf " + std::to_string(count + 1) + "/" + std::to_string(number_of_leafs) + "; start with new \"sum cloud\"");
+      }
+      size_t use_cloud_number = count / number_of_clouds_to_be_summed_into_one;
+      std::shared_ptr<pcl::PCLPointCloud2> cloud_merged_tmp = std::make_shared<pcl::PCLPointCloud2>();
+      pcl::concatenatePointCloud(*clouds_filtered.at( use_cloud_number ), pc_voxelgrid_cloud, *cloud_merged_tmp);
+      clouds_filtered.at( use_cloud_number ) = cloud_merged_tmp;
     }
 
-    log_info( "new pointcloudsize " << cloud_filtered->width << "(leafsize: " << leafSize << ")" );
+    log_info("opereator filter_voxelgrid: finished filtering, fuse " + std::to_string(number_of_sum_clouds) + " \"sum clouds\" together for final result");
+    // merge two clouds next to each other together untill there is only one cloud left
+    while (clouds_filtered.size() > 1) {
+        log_info("opereator filter_voxelgrid: fuse " + std::to_string(clouds_filtered.size()) + " clouds");
+        std::vector<std::shared_ptr<pcl::PCLPointCloud2>> clouds_filtered_new;
+        for (  size_t i = 1; i < clouds_filtered.size(); i += 2) {
+            // merge this and the previous cloud
+            std::shared_ptr<pcl::PCLPointCloud2> merged_cloud = std::make_shared<pcl::PCLPointCloud2>();
+            pcl::concatenatePointCloud(*clouds_filtered.at(i - 1), *clouds_filtered.at(i), *merged_cloud);
 
-    entityData->setData(cloud_filtered);
+            // push back merged cloud to new array
+            clouds_filtered_new.push_back( merged_cloud );
+        }
+        // add the last cloud that might be missed by the loop
+        if ( (clouds_filtered.size() - 1) % 2 == 0) {
+            clouds_filtered_new.push_back( clouds_filtered.back() );
+        }
+
+        // do the same with the new array
+        clouds_filtered = clouds_filtered_new;
+    }
+
+    log_info( "opereator filter_voxelgrid: new pointcloudsize " << clouds_filtered.front()->width << " (leafsize: " << leafSize << ")" );
+
+    entityData->setData(clouds_filtered.front());
 
     return MAPIT_STATUS_OK;
 }
