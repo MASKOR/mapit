a29584f4209d31e9c77e695853798a4a1cd56403
Tobias Neumann <t.neumann@fh-aachen.de>
2018-08-06 17:24:14 +0200

    operator/octomap2occupancy_grid: first working parts
    to create the 2D map from the 3D map, this operator creats slices in the
    3D map at each scanpoint of the scans that are used to create this map.
    
    The creation of the slices seems to work (has been printed ansii art
    like to the console). However the 2D map is not yet defined and
    therefore the slices can not be put together.

--- libs/operator_modules_collection/octomap2occupancygrid/src/main.cpp
+++ libs/operator_modules_collection/octomap2occupancygrid/src/main.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  *
  * Copyright 2016-2017 Daniel Bulla	<d.bulla@fh-aachen.de>
- *           2016-2017 Tobias Neumann	<t.neumann@fh-aachen.de>
+ *           2016-2018 Tobias Neumann	<t.neumann@fh-aachen.de>
  *
 ******************************************************************************/
 
@@ -23,9 +23,11 @@
 
 #include <mapit/operators/module.h>
 #include <mapit/logging.h>
-#include <mapit/layertypes/tflayer.h>
+#include <mapit/layertypes/tflayer/tf2/buffer_core.h>
 #include <mapit/operators/versioning/workspacewritable.h>
 #include <mapit/operators/operationenvironment.h>
+#include <mapit/depthfirstsearch.h>
+#include <mapit/layertypes/octomaplayer.h>
 #include <iostream>
 #include <memory>
 #include <mapit/errorcodes.h>
@@ -34,118 +36,211 @@
 #include <QtCore/QJsonObject>
 #include <QtCore/QJsonArray>
 
-mapit::StatusCode operate_load_tfs(mapit::OperationEnvironment* env)
+std::vector<mapit::time::Stamp>
+get_param_input(mapit::OperationEnvironment* env, const QJsonObject& params)
+{
+    std::vector<mapit::time::Stamp> stamps;
+    std::list<std::string> input_list;
+    if        ( params["path-entity-stamps"].isString() ) {
+        input_list.push_back( params["path-entity-stamps"].toString().toStdString() );
+    } else if ( params["path-entity-stamps"].isArray() ) {
+        for (QJsonValue input : params["path-entity-stamps"].toArray() ) {
+            if ( ! input.isString() ) {
+                log_error("operator octomap2occupancy_grid: cfg \"path-entity-stamps\" is not an array of strings");
+                throw MAPIT_STATUS_INVALID_ARGUMENT;
+            }
+            input_list.push_back( input.toString().toStdString() );
+        }
+    } else {
+        log_error("operator octomap2occupancy_grid: cfg \"path-entity-stamps\" does not is a string or array of strings");
+        throw MAPIT_STATUS_INVALID_ARGUMENT;
+    }
+
+    // search for all in input
+    // if entity -> add to list
+    // if tree -> depth search, add all entities to list
+    // otherwise error
+    for (std::string input_name : input_list) {
+        std::shared_ptr<mapit::msgs::Entity> entity = env->getWorkspace()->getEntity(input_name);
+        if (entity != nullptr) {
+            stamps.push_back( mapit::time::from_msg(entity->stamp()) );
+        } else {
+            if (env->getWorkspace()->getTree(input_name) != nullptr) {
+                env->getWorkspace()->depthFirstSearch(
+                              input_name
+                            , depthFirstSearchWorkspaceAll(mapit::msgs::Tree)
+                            , depthFirstSearchWorkspaceAll(mapit::msgs::Tree)
+                            , [&](std::shared_ptr<mapit::msgs::Entity> obj, const ObjectReference& ref, const mapit::Path &path)
+                              {
+                                  stamps.push_back( mapit::time::from_msg(obj->stamp()) );
+                                  return true;
+                              }
+                            , depthFirstSearchWorkspaceAll(mapit::msgs::Entity)
+                            );
+            } else {
+                log_error("operator octomap2occupancy_grid: entity name \"" + input_name + "\" given in param \"path-entity-stamps\", is neither a entity nor a tree");
+                throw MAPIT_STATUS_ERR_DB_INVALID_ARGUMENT;
+            }
+        }
+    }
+
+    return stamps;
+}
+
+mapit::StatusCode operate_octomap2opccupancy_grid(mapit::OperationEnvironment* env)
 {
     /** structure:
      * {
-     *  "prefix" : ...,
-     *  "transforms" :
-     *  [                       [repeated]
-     *      {
-     *          "static" : ...,
-     *          "header" : {
-     *              "frame_id" : ...,
-     *              "stamp" : {
-     *                  "sec" : ...,
-     *                  "nsec" : ...
-     *              }
-     *          },
-     *
-     *          "transform" : {
-     *              "child_frame_id" : ...,
-     *              "translation" : {
-     *                  "x" : ...,
-     *                  "y" : ...,
-     *                  "z" : ...
-     *              },
-     *              "rotation" : {
-     *                  "w" : ...,
-     *                  "x" : ...,
-     *                  "y" : ...,
-     *                  "z" : ...
-     *              }
-     *          }
-     *      },
-     *      {
-     *          ...
-     *      }
-     *  ]
+     *  <string>"tf-prefix" : ...,              // default ""
+     *  <string>"sensor-frame_id" : ...,
+     *  <string>"path-octomap" : ...,
+     *  <string>"path-occupancy_grid" : ...,
+     *  <string>"path-entity-stamps" : ...,     // the stamps of this entities will be used to transform 3D to 2D (typicly the data that where used to generate the map)
+     *  <float>"sensor-distance" : ...,
+     *  <float>"slice-hight" : ...              // default 0.1
      * }
      */
     QJsonDocument paramsDoc = QJsonDocument::fromJson( QByteArray(env->getParameters().c_str(), env->getParameters().length()) );
     QJsonObject params(paramsDoc.object());
 
-    std::string prefix = params["prefix"].toString().toStdString();
+    // -- get configs
+    std::string tf_prefix = params["tf-prefix"].toString("").toStdString();
+    if ( ! params.contains("sensor-frame_id") ) {
+        log_error("operator octomap2occupancy_grid: config does not contain sensor-frame_id");
+        return MAPIT_STATUS_ERR_DB_INVALID_ARGUMENT;
+    }
+    std::string frame_id_sensor = params["sensor-frame_id"].toString().toStdString();
+    if ( ! params.contains("path-octomap") ) {
+        log_error("operator octomap2occupancy_grid: config does not contain path-octomap");
+        return MAPIT_STATUS_ERR_DB_INVALID_ARGUMENT;
+    }
+    std::string path_octomap = params["path-octomap"].toString().toStdString();
+    if ( ! params.contains("path-entity-stamps") ) {
+        log_error("operator octomap2occupancy_grid: config does not contain path-entity-stamps");
+        return MAPIT_STATUS_ERR_DB_INVALID_ARGUMENT;
+    }
+    // -- get entity for stamps
+    std::vector<mapit::time::Stamp> stamps;
+    try {
+        stamps = get_param_input(env, params);
+    } catch (mapit::StatusCode status) {
+        return status;
+    }
+    float distance_sensor = static_cast<float>( params["sensor-distance"].toDouble( static_cast<double>(std::numeric_limits<float>::max()) ) );
+    float slice_hight = static_cast<float>( params["slice-hight"].toDouble(0.1) );
 
+    // -- get workspace and transform buffer
     mapit::operators::WorkspaceWritable* workspace = env->getWorkspace();
 
-    std::shared_ptr<mapit::tf::store::TransformStampedListGatherer> tfs_map
-        = std::make_shared<mapit::tf::store::TransformStampedListGatherer>();
+    mapit::tf2::BufferCore tf_buffer(workspace, tf_prefix);
 
-    // TODO check if data is available
-    QJsonArray json_transforms( params["transforms"].toArray() );
-    for ( QJsonValue json_value_tf : json_transforms ) {
-        QJsonObject json_tf( json_value_tf.toObject() );
+    // -- get entity octomap
+    std::shared_ptr<mapit::msgs::Entity> octomap_entity = workspace->getEntity(path_octomap);
+    if ( ! octomap_entity ) {
+        log_error("operator octomap2occupancy_grid: entity \"" << path_octomap << "\" does not exists.");
+        return MAPIT_STATUS_ERR_DB_INVALID_ARGUMENT;
+    }
+    if ( 0 != octomap_entity->type().compare(mapit::entitytypes::Octomap::TYPENAME()) ) {
+        log_error("operator octomap2occupancy_grid: entity \"" << path_octomap << "\" is of type " << octomap_entity->type()
+                                                               << " but " << mapit::entitytypes::Octomap::TYPENAME() << " is required.");
+        return MAPIT_STATUS_ERR_DB_INVALID_ARGUMENT;
+    }
+    std::string frame_id_map = octomap_entity->frame_id();
+    std::shared_ptr<mapit::entitytypes::Octomap> octomap_entitydata = std::static_pointer_cast<mapit::entitytypes::Octomap>( workspace->getEntitydataReadOnly(path_octomap) );
 
-        bool is_static = json_tf["static"].toBool();
+    Eigen::Affine3f pose;
+    Eigen::Affine2f pose_2d; // for the 3D to 2D mapping, this is only x, y, ori
+    // -- for each stamp
+    for (mapit::time::Stamp stamp : stamps) {
+        // -- get pose from transform
+        try {
+            mapit::tf::TransformStamped tf = tf_buffer.lookupTransform(frame_id_map, frame_id_sensor, stamp);
+            pose = tf.transform.rotation * tf.transform.translation;
+            Eigen::Vector3f euler = tf.transform.rotation.toRotationMatrix().eulerAngles(0, 1, 2);
+            Eigen::Rotation2Df rot_2d(euler[2]);
+            Eigen::Translation2f trans_2d(  tf.transform.translation.x()
+                                          , tf.transform.translation.y());
+            pose_2d = rot_2d * trans_2d;
+        } catch (...) {
+            log_error("operator octomap2occupancy_grid: can't lookup transform " << frame_id_map << " -> " << frame_id_sensor
+                                                                                 << " at time " << mapit::time::to_string(stamp));
+            return MAPIT_STATUS_ERR_UNKNOWN;
+        }
+        std::shared_ptr<octomap::OcTree> octomap = octomap_entitydata->getData();
+        double octomap_resolution = octomap->getResolution();
+        // -- for all points in the transformed plane
+        // TODO does this work, or do I have to use the transformed x, y, z
+        double om_m_x, om_m_y, om_m_z;
+        float octomap_max_x, octomap_max_y, octomap_max_z;
+        octomap->getMetricMax(om_m_x, om_m_y, om_m_z);
+        octomap_max_x = static_cast<float>(om_m_x);
+        octomap_max_y = static_cast<float>(om_m_y);
+        octomap_max_z = static_cast<float>(om_m_z);
 
-        // get data from json
-        std::unique_ptr<mapit::tf::TransformStamped> tf_loaded = std::unique_ptr<mapit::tf::TransformStamped>(new mapit::tf::TransformStamped);
+//        // Test export to ansii art on screen, only works with 10x10m with 0.5 resolution
+//        std::map<size_t, std::vector<std::string>> map;
+//        size_t width = 41;
+//        for (size_t x = 0; x < width; ++x) {
+//            map[x] = std::vector<std::string>();
+//            for (size_t y = 0; y < width; ++y) {
+//                map.at(x).push_back(" ");
+//            }
+//        }
 
-        // get header
-        QJsonObject header( json_tf["header"].toObject() );
-        tf_loaded->frame_id = header["frame_id"].toString().toStdString();
-        QJsonObject stamp( header["stamp"].toObject() );
-        // TODO when int is not enough we can use string and cast this to long (jason does not support long)
-        tf_loaded->stamp = mapit::time::from_sec_and_nsec(stamp["sec"].toInt(), stamp["nsec"].toInt());
+        for (float x = 0; x <= distance_sensor && x < octomap_max_x; x += octomap_resolution) {
+            for (float y = 0; y <= distance_sensor && y < octomap_max_y; y += octomap_resolution) {
+                float z = 0; {
+//                for (float z = 0; z <= slice_hight && z < octomap_max_z; z += octomap_resolution) {
+                    // check all 8 poses (+/-x, +/-y and +/-z)
+                    std::vector<Eigen::Vector3f> vecs;
+                    vecs.push_back( Eigen::Vector3f( x,  y,  z) );
+                    vecs.push_back( Eigen::Vector3f( x,  y, -z) );
+                    vecs.push_back( Eigen::Vector3f( x, -y,  z) );
+                    vecs.push_back( Eigen::Vector3f( x, -y, -z) );
+                    vecs.push_back( Eigen::Vector3f(-x,  y,  z) );
+                    vecs.push_back( Eigen::Vector3f(-x,  y, -z) );
+                    vecs.push_back( Eigen::Vector3f(-x, -y,  z) );
+                    vecs.push_back( Eigen::Vector3f(-x, -y, -z) );
+                    for (Eigen::Vector3f vec : vecs) {
+                        // transform point on plane
+                        Eigen::Vector3f vec_tf = pose * vec;
+                        // get node
+                        octomap::OcTreeNode *node = octomap->search(  static_cast<double>(vec_tf.x())
+                                                                    , static_cast<double>(vec_tf.y())
+                                                                    , static_cast<double>(vec_tf.z())
+                                                                    );
+                        // get greyvalue 0 = occupied, 128 = unknown, 255 = free
+                        size_t greyvalue = 128; // unknown
+                        if ( node ) {
+                            if ( octomap->isNodeOccupied(node) ) {
+                                greyvalue = 0;
+                            } else {
+                                greyvalue = 255;
+                            }
+                        }
 
-        // get data
-        QJsonObject json_transform( json_tf["transform"].toObject() );
-        tf_loaded->child_frame_id = json_transform["child_frame_id"].toString().toStdString();
-        QJsonObject json_translation( json_transform["translation"].toObject() );
-        QJsonObject json_rotation( json_transform["rotation"].toObject() );
-        tf_loaded->transform.translation = Eigen::Translation3f(
-                    (float)json_translation["x"].toDouble()
-                ,   (float)json_translation["y"].toDouble()
-                ,   (float)json_translation["z"].toDouble()
-                    );
-
-        tf_loaded->transform.rotation = Eigen::Quaternionf(
-                    (float)json_rotation["w"].toDouble()
-                ,   (float)json_rotation["x"].toDouble()
-                ,   (float)json_rotation["y"].toDouble()
-                ,   (float)json_rotation["z"].toDouble()
-                    );
-
-        tf_loaded->transform.translation = Eigen::Translation3f(
-                    (float)json_translation["x"].toDouble()
-                ,   (float)json_translation["y"].toDouble()
-                ,   (float)json_translation["z"].toDouble()
-                    );
-        tf_loaded->transform.rotation.normalize();
-
-        bool valid = std::abs((tf_loaded->transform.rotation.w() * tf_loaded->transform.rotation.w()
-                             + tf_loaded->transform.rotation.x() * tf_loaded->transform.rotation.x()
-                             + tf_loaded->transform.rotation.y() * tf_loaded->transform.rotation.y()
-                             + tf_loaded->transform.rotation.z() * tf_loaded->transform.rotation.z()) - 1.0f) < 10e-6;
-        if(!valid)
-        {
-            log_warn("Invalid Quaternion 0.");
-            return MAPIT_STATUS_ERROR;
+                        // transform point on plane to 2D map from pose(x, y, ori) and vec(x, y)
+                        Eigen::Vector2f vec_tf_2d = pose_2d * Eigen::Vector2f(vec.x(), vec.y());
+                        log_info("write at ( " << vec_tf_2d.transpose() << " ) = " << greyvalue);
+//                        // Test export to ansii art on screen
+//                        map.at(vec.x()/0.5 + 10/0.5).at(vec.y()/0.5 + 10/0.5) = greyvalue;
+                    }
+                }
+            }
         }
 
-        // add data to map
-        tfs_map->add_transform( std::move( tf_loaded ), is_static );
+        // Test export to ansii art on screen
+//        for (size_t x = 0; x < width; ++x) {
+//            std::string line = "";
+//            for (size_t y = 0; y < width; ++y) {
+//                line += map.at(x).at(y) + " ";
+//            }
+//            log_info( line );
+//        }
+//        log_info("");
     }
 
-    mapit::StatusCode usc_static = tfs_map->store_entities(workspace, prefix);
-
-    if (   usc_static == MAPIT_STATUS_OK) {
-      return MAPIT_STATUS_OK;
-    } else {
-      return usc_static;
-    }
-
+    return MAPIT_STATUS_OK;
 }
 
-MAPIT_MODULE(OPERATOR_NAME, "store transforms in mapit", "fhac", OPERATOR_VERSION, "any", true, &operate_load_tfs)
+MAPIT_MODULE(OPERATOR_NAME, "convert an octree to and occupancy grid (3D map -> 2D map)", "fhac", OPERATOR_VERSION, "any", true, &operate_octomap2opccupancy_grid)
